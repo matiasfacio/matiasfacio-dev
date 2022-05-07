477e565a3ea37af2ef5cdfca0c8530bed119eb13
Matias Facio <matiaspersonal@gmail.com>
2022-05-08 00:51:28 +0200

    types2

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -11,24 +11,23 @@ const Footer = () => {
           <a href="tel:00491774946117">Phone: +49 177 4946117</a>
         </div>
         <a href="mailto:matiaspersonal@gmail.com">matiaspersonal@gmail.com</a>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "5px",
-            cursor: "pointer",
-          }}
-          // @ts-expect-error ts-migrate(2322) FIXME: Type 'string' is not assignable to type 'Location'... Remove this comment to see the full error message
-          onClick={() => (window.location = "https://github.com/matiasfacio/")}
-        >
+        <AnchorLink href="https://github.com/matiasfacio/">
           <img src={github} width="30px" alt="github" /> Github
-        </div>
+        </AnchorLink>
       </FooterBottom>
     </FooterContainer>
   );
 };
 
 export default Footer;
+
+const AnchorLink = styled.a`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+  text-decoration: none !important;
+`;
 
 const FooterContainer = styled.footer`
   position: relative;
--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -21,7 +21,6 @@ const Nav = () => {
           alt="menu"
         />
       </LogoMenu>
-      // @ts-expect-error ts-migrate(2769) FIXME: No overload matches this call.
       <NavBar visible={menuVisibility}>
         <ul>
           <Link to="/" onClick={handleClick}>
@@ -47,7 +46,7 @@ const Nav = () => {
 
 export default Nav;
 
-const NavBar = styled.nav`
+const NavBar = styled.nav<{ visible?: boolean }>`
   position: fixed;
   top: 0;
   right: 0;
@@ -95,7 +94,6 @@ const NavBar = styled.nav`
       width: 100%;
       transition: all 0.5s ease-in-out;
 
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'visible' does not exist on type 'Pick<De... Remove this comment to see the full error message
       ${({ visible }) =>
         visible
           ? "opacity: 1; transform: translateX(0); z-index: 100;"
@@ -108,7 +106,6 @@ const NavBar = styled.nav`
     justify-content: center;
     transition: all 0.5s ease-in-out;
 
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'visible' does not exist on type 'Pick<De... Remove this comment to see the full error message
     ${({ visible }) =>
       visible
         ? "opacity:1; transform: translateX(0);z-index: 100;"
